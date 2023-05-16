0eb17b5c19d3d34cc363edfc387a37fba41ce6cd
sergey-emelyanov <emelia35@yandex.ru>
2023-05-16 21:49:32 +0300

    some changes

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,14 @@
-from flask import Flask,render_template
+import os
+
+import psycopg2
+from flask import Flask, render_template
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.route('/')
 def index():
