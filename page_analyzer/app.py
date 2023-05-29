99b4a8976601c2c7b52b498fc0ae9067a8d4926f
sergey-emelyanov <emelia35@yandex.ru>
2023-05-29 21:04:33 +0300

    step 3 part1

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 import os
-
 import psycopg2
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request, url_for
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -9,7 +9,54 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 conn = psycopg2.connect(DATABASE_URL)
+cur = conn.cursor()
+
+
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.post('/')
+def post_urls():
+    conn = get_db_connection()
+    url = request.form['url']
+    date = datetime.date.today()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO urls(name,created_at)'
+                'VALUES(%s,%s)',
+                (url, date)
+                )
+    conn.commit()
+    cur.close()
+    conn.close()
+    return url_for('index')
+
+
+@app.route('/urls')
+def show_urls():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM urls;')
+    urls = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('show_all.html', urls=urls)
+
+
+@app.route('/urls/<int:id>')
+def show_one(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(f'SELECT * FROM urls WHERE id={id}')
+    url = cur.fetchone()
+    cur.close()
+    conn.close()
+    return render_template('show_one.html', url=url)
+
+
