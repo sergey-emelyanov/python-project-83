640678478c6b5379cc2b3681ff57af485738ee79
sergey-emelyanov <emelia35@yandex.ru>
2023-06-27 18:56:50 +0300

    add errorhandler

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -81,3 +81,16 @@ def check_url(id):
                        status_code, h1, title, content)
     flash("Страница успешно проверена", "success")
     return redirect(url_for('show_one', id=id))
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
+
